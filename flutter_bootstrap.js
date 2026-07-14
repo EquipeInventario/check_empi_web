f4d5a8f10718fc941f0c2a1cc3f8767108b641f8
EquipeInventario <inventario.pg@mastercargasbrasil.com.br>
2026-07-14 12:20:36 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1539033356" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2683278018" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
